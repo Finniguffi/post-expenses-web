2a7dc7e639344b227138be849bdae1a39a862d87
Finniguffi <finniguffi@gmail.com>
2025-02-06 16:18:41 -0300

    feat: update Angular dependencies, add Lucide icons, and enhance input component with password visibility toggle

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,18 +1,60 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { LucideAngularModule, Eye, EyeOff } from 'lucide-angular';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, LucideAngularModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() type: string = 'text';
   @Input() id: string = '';
   @Input() label: string = '';
-  @Input() model: any;
   @Input() name: string = '';
+
+  value: any;
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  isPasswordVisible: boolean = false;
+  
+  // Icons 
+  readonly Eye = Eye;
+  readonly EyeOff = EyeOff;
+
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    // Handle the disabled state if needed
+  }
+
+  togglePasswordVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  get inputType(): string {
+    return this.type === 'password' && this.isPasswordVisible ? 'text' : this.type;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { provideAnimations } from '@angular/platform-browser/animations'; 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    ...appConfig.providers
+    ...appConfig.providers,
+    provideAnimations(),
   ]
 }).catch((err) => console.error(err));
